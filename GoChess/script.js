9776fcf3bdd60bbdbe0d78aee615329cbb6b404c
chawai <74479299+chawaisir@users.noreply.github.com>
2021-11-18 21:57:09 +0800

    试试

---
+++ GoChess/script.js
@@ -0,0 +1,8 @@
+$('#toggle').click(function() {
+    $(this).toggleClass('active');
+    $('#overlay').toggleClass('open');
+   });
+//    it is the action for active and open the hamburger menu
+//    the js code is from Paul van Oijen full menu code
+//    /* Copyright (c) 2021 by Paul van Oijen (https://codepen.io/PaulVanO/pen/XJYGNQ)
+//    view the orignal work https://codepen.io/PaulVanO/pen/XJYGNQ */
